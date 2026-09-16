5f876f274d66b2e716cd793a1db781893ef7a853
rathaROG <siv.ratha89@gmail.com>
2026-09-16 17:08:54 +0200

    chore: bump version to 0.2.0 (10)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rsgkh.calendar"
         minSdk = 31
         targetSdk = 37
-        versionCode = 9
-        versionName = "0.1.8"
+        versionCode = 10
+        versionName = "0.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
