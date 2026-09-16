a0ea6ef408a43c2850d0d9b5d42140bc1590aa02
rathaROG <siv.ratha89@gmail.com>
2026-09-16 18:42:04 +0200

    Bump version to 0.2.1 build 11

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rsgkh.calendar"
         minSdk = 31
         targetSdk = 37
-        versionCode = 10
-        versionName = "0.2.0"
+        versionCode = 11
+        versionName = "0.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
