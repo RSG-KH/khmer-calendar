c4e783a6d270aebfc9788e70afd28d7442a64f70
rathaROG <siv.ratha89@gmail.com>
2026-09-18 04:01:05 +0200

    Bump version to 0.4.1 build 16

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rsgkh.calendar"
         minSdk = 31
         targetSdk = 37
-        versionCode = 15
-        versionName = "0.4.0"
+        versionCode = 16
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
