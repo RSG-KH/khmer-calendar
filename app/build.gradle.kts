05df5703833e0553b61ad42240dc6191eb4301b3
rathaROG <siv.ratha89@gmail.com>
2026-09-18 18:35:17 +0200

    Bump version to 0.6.0 build 18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.rsgkh.calendar"
         minSdk = 31
         targetSdk = 37
-        versionCode = 17
-        versionName = "0.5.0"
+        versionCode = 18
+        versionName = "0.6.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
